3ece54ba3b30cf38c1525732dab875b8d6a4d5b4
Xerux <Boggart@users.noreply.github.com>
2016-11-29 01:53:59 -0700

    Update first_run.sh

--- scripts/first_run.sh
+++ scripts/first_run.sh
@@ -16,16 +16,9 @@ pre_start_action() {
 
   # Ensure mysql owns the DATA_DIR
   chown -R mysql $DATA_DIR
-  chown root $DATA_DIR/debian*.flag
 }
 
 post_start_action() {
-  # The password for 'debian-sys-maint'@'localhost' is auto generated.
-  # The database inside of DATA_DIR may not have been generated with this password.
-  # So, we need to set this for our database to be portable.
-  DB_MAINT_PASS=$(cat /etc/mysql/debian.cnf | grep -m 1 "password\s*=\s*"| sed 's/^password\s*=\s*//')
-  mysql -u root -e \
-      "GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '$DB_MAINT_PASS';"
 
   # Create the superuser.
   mysql -u root <<-EOF
